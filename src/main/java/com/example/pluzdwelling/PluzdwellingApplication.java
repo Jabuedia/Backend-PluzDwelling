e783b77118a5560d9259479acad51106acfff5d9
Jbuendia <jabuendia19@gmail.com>
2022-10-25 19:14:50 -0500

    Commit inicial del proyecto backend en gestion de apartamentos.JB

---
+++ src/main/java/com/example/pluzdwelling/PluzdwellingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pluzdwelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluzdwellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluzdwellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pluzdwelling/PluzdwellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pluzdwelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluzdwellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
